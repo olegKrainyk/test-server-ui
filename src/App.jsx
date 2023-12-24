773235be253f588eb3f3295457fd74d45249473a
olegKrainyk <oleg.kr944@gmail.com>
2023-12-23 22:31:41 -0500

    io many requests fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 import Login from './Login/Login'
 import { Route, Routes } from 'react-router-dom'
@@ -9,17 +9,20 @@ import { useNavigate } from 'react-router-dom'
 export default function App(){
 
   const navigate = useNavigate()
+
   const socket = io('http://10.241.104.202:8888')
 
-  socket.on("logout", response => {
-    console.log('deleting session ' + response);
-    
-    if(response == user.sessionid && response !== null && user.sessionid !== null) {
-      localStorage.setItem("user", '[{}]')
-      navigate('/')
-      setUser([{}]);
-    }
-  })
+  useEffect(() => {
+    socket.on("logout", response => {
+      console.log('deleting session ' + response);
+      
+      if(response == user.sessionid && response !== null && user.sessionid !== null) {
+        localStorage.setItem("user", '[{}]')
+        navigate('/')
+        setUser([{}]);
+      }
+    })
+  }, []);
 
   const [user, setUser] = useState(localStorage.getItem("user") !== null ? JSON.parse(localStorage.getItem("user")) : '[{}]');
 
--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -8,19 +8,6 @@ export default function Profile(props){
 
   const navigate = useNavigate()
 
-  // useEffect(() => {
-  //   axios.get(`http://10.241.104.202:8080/check-session`, {params: {sessionid: props.user.sessionid, id: props.user.id}})
-  //   .then(response => {
-  //       console.log(response.data.active);
-  //       if(response.data.active === false) {
-  //         handleLogOut();
-  //       }
-  //   })
-  //   .catch(error => {
-  //       console.error('There was an error!', error);
-  //   });
-  // });
-
   const handleLogOut = () => {
 
     axios.get(`http://10.241.104.202:8080/log-out-session`, {params: {sessionid: props.user.sessionid, id: props.user.id}})
